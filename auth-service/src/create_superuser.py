d099dd68878904d221c05cd0e5ed21fe994fb78c
Ekaterina <cherednichenko_kate@mail.ru>
2024-05-31 09:14:46 +0300

    refactor script for superuser creation

--- auth-service/src/create_superuser.py
+++ auth-service/src/create_superuser.py
@@ -8,10 +8,8 @@ from core.config import settings
 SUPERUSER = 'superuser'
 
 if __name__ == '__main__':
-    print('Enter login:')
-    username = input()
-    print('Enter password:')
-    password = input()
+    username = input('Enter login: ')
+    password = input('Enter password: ')
     salt = username.encode('utf-8')
     hashed_password = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000).hex()
 
@@ -26,14 +24,15 @@ if __name__ == '__main__':
                 (str(user_id), username, hashed_password, datetime.now()))
     cur.execute(f"select id from roles where name = '{SUPERUSER}'")
     role_id = cur.fetchone()
+    message = ''
     if role_id:
         cur.execute('insert into user_role values(%s, %s)', (str(user_id), role_id))
-        message = f'added superuser with id = {user_id}'
     else:
         role_id = uuid.uuid4()
         cur.execute('insert into roles values(%s, %s, %s)', (str(role_id), SUPERUSER, datetime.now()))
         message = f'added role superuser with id = {role_id}\n'
         cur.execute('insert into user_role values(%s, %s)', (str(user_id), str(role_id)))
-        message += f'added superuser with id = {user_id}'
+    message += f'added superuser with id = {user_id}'
     conn.commit()
     print(message)
+    conn.close()
