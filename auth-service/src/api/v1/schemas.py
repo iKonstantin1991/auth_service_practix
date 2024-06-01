7d02449db380ab37f7ade72a76e0f104f3214c21
iKonstantin1991 <ikonstantin1991@mail.ru>
2024-06-01 11:11:16 +0300

    move schemas to api module

---
+++ auth-service/src/api/v1/schemas.py
@@ -0,0 +1,40 @@
+from uuid import UUID
+from datetime import datetime
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserCredentials(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserIn(UserCredentials):
+    pass
+
+
+class UserOut(BaseModel):
+    id: UUID
+    email: EmailStr
+
+    class Config:
+        from_attributes = True
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+
+
+class AuthHistory(BaseModel):
+    user_agent: str
+    date: datetime
+
+
+class RoleIn(BaseModel):
+    name: str
+
+
+class RoleOut(BaseModel):
+    id: UUID
+    name: str
