e552d3367f53406b6c85d75c773421e2fc77554e
iKonstantin1991 <ikonstantin1991@mail.ru>
2024-06-05 15:28:54 +0300

    add user tests (#21)

--- auth-service/tests/functional/plugins/users.py
+++ auth-service/tests/functional/plugins/users.py
@@ -1,73 +1,51 @@
 from typing import Iterator, List
 from uuid import uuid4, UUID
 import hashlib
+from dataclasses import dataclass
 
 import pytest_asyncio
-from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from tests.functional.plugins.models import User, Role
-from tests.functional.plugins.roles import TestRole
 
 
-class TestUser(BaseModel):
+@dataclass(frozen=True)
+class TestUser:
     __test__ = False
 
+    id: UUID
     email: str
     password: str
-
-
-class TestUserWithRoles(BaseModel):
-    __test__ = False
-
-    email: str
-    password: str
-    roles: List[TestRole]
+    roles: List[Role]
 
 
 @pytest_asyncio.fixture(name='user')
 async def fixture_user(db_session: AsyncSession) -> Iterator[TestUser]:
-    email = f'{uuid4()}@test.com'
-    password = 'password'
+    id, email, password = uuid4(), f'{uuid4()}@test.com', 'password'
+    roles = [Role(id=uuid4(), name=str(uuid4()))]
     user = User(
-        id=uuid4(),
+        id=id,
         email=email,
         hashed_password=_hash_password(email, password),
+        roles=roles,
     )
     db_session.add(user)
     await db_session.commit()
-    yield TestUser(email=email, password=password)
+    yield TestUser(id=id, email=email, password=password, roles=roles)
 
 
 @pytest_asyncio.fixture(name='superuser')
 async def fixture_superuser(db_session: AsyncSession, superuser_role: Role) -> Iterator[TestUser]:
-    email = f'{uuid4()}@test.com'
-    password = 'password'
+    id, email, password = uuid4(), f'{uuid4()}@test.com', 'password'
     user = User(
-        id=uuid4(),
+        id=id,
         email=email,
         hashed_password=_hash_password(email, password),
         roles=[superuser_role],
     )
     db_session.add(user)
     await db_session.commit()
-    yield TestUser(id=user_id, email=email, password=password)
-
-
-@pytest_asyncio.fixture(name='user_with_usual_role')
-async def fixture_user_with_usual_role(db_session, usual_user_role) -> Iterator[TestUserWithRoles]:
-    user_id = uuid4()
-    email = f'{uuid4()}@test.com'
-    password = 'password'
-    user = User(
-        id=user_id,
-        email=email,
-        hashed_password=_hash_password(email, password),
-        roles=[usual_user_role],
-    )
-    db_session.add(user)
-    await db_session.commit()
-    yield usual_user_role
+    yield TestUser(id=id, email=email, password=password, roles=[superuser_role])
 
 
 def _hash_password(salt: str, password: str) -> str:
--- auth-service/tests/functional/src/test_users.py
+++ auth-service/tests/functional/src/test_users.py
@@ -4,14 +4,14 @@ from uuid import uuid4
 import pytest
 
 from tests.functional.conftest import Client
-from tests.functional.plugins.users import TestUser, TestUserWithRoles
-from tests.functional.plugins.roles import TestRole
+from tests.functional.plugins.users import TestUser
+from tests.functional.plugins.roles import Role
 from tests.functional.src.utils import build_headers, login
 
 
 @pytest.mark.asyncio
 async def test_get_user_success(client: Client, user: TestUser) -> None:
-    access_token, _ = await login(user, client, user_agent=f'test user agent {uuid4()}')
+    access_token, _ = await login(user, client)
 
     response = await client.get(f'api/v1/users/{user.id}', headers=build_headers(access_token))
 
@@ -22,18 +22,17 @@ async def test_get_user_success(client: Client, user: TestUser) -> None:
 
 
 @pytest.mark.asyncio
-async def test_get_user_without_permission(client: Client, user: TestUser) -> None:
-    user_to_get = TestUser(id=uuid4(), email='user2@test.com', password='password')
-    access_token, _ = await login(user, client, user_agent=f'test user agent {uuid4()}')
+async def test_get_user_without_permission(client: Client, user: TestUser, superuser: TestUser) -> None:
+    access_token, _ = await login(user, client)
 
-    response = await client.get(f'api/v1/users/{user_to_get.id}', headers=build_headers(access_token))
+    response = await client.get(f'api/v1/users/{superuser.id}', headers=build_headers(access_token))
 
     assert response.status == HTTPStatus.FORBIDDEN
 
 
 @pytest.mark.asyncio
 async def test_update_user_success(client: Client, user: TestUser) -> None:
-    access_token, _ = await login(user, client, user_agent=f'test user agent {uuid4()}')
+    access_token, _ = await login(user, client)
     email_to_update = 'new_email@test.com'
     password_to_update = 'new_password'
 
@@ -50,17 +49,42 @@ async def test_update_user_success(client: Client, user: TestUser) -> None:
 
 
 @pytest.mark.asyncio
-async def test_update_user_without_permission(client: Client, user: TestUser) -> None:
-    user_to_update = TestUser(id=uuid4(), email='user2@test.com', password='password')
-    access_token, _ = await login(user, client, user_agent=f'test user agent {uuid4()}')
+async def test_update_user_without_permission(client: Client, user: TestUser, superuser: TestUser) -> None:
+    access_token, _ = await login(user, client)
     email_to_update = 'new_email@test.com'
     password_to_update = 'new_password'
 
     response = await client.put(
-        f'api/v1/users/{user_to_update.id}',
+        f'api/v1/users/{superuser.id}',
         body={'email': email_to_update, 'password': password_to_update},
         headers=build_headers(access_token)
     )
+
+    assert response.status == HTTPStatus.FORBIDDEN
+
+
+@pytest.mark.asyncio
+async def test_get_user_roles_returns_roles_superuser(
+    client: Client, superuser: TestUser, user: TestUser
+) -> None:
+    access_token, _ = await login(superuser, client)
+
+    response = await client.get(f'api/v1/users/{user.id}/roles', headers=build_headers(access_token))
+
+    assert response.status == HTTPStatus.OK
+    body = await response.json()
+    assert len(body) == len(user.roles)
+    assert body[0]['id'] == str(user.roles[0].id)
+    assert body[0]['name'] == user.roles[0].name
+
+
+@pytest.mark.asyncio
+async def test_get_user_roles_returns_forbidden_regular_user(
+    client: Client, user: TestUser, superuser: TestUser
+) -> None:
+    access_token, _ = await login(user, client)
+
+    response = await client.get(f'api/v1/users/{superuser.id}/roles', headers=build_headers(access_token))
 
     assert response.status == HTTPStatus.FORBIDDEN
 
@@ -70,32 +94,32 @@ async def test_assign_existing_role_to_another_by_superuser(
         client: Client,
         superuser: TestUser,
         user: TestUser,
-        usual_user_role: TestRole
+        role: Role
 ) -> None:
-    access_token, _ = await login(superuser, client, user_agent=f'test user agent {uuid4()}')
+    access_token, _ = await login(superuser, client)
 
     response = await client.post(
-        f'api/v1/users/{user.id}/roles?role_id={usual_user_role.id}',
+        f'api/v1/users/{user.id}/roles?role_id={role.id}',
         headers=build_headers(access_token)
     )
 
     assert response.status == HTTPStatus.OK
     body = await response.json()
-    assert body['id'] == str(usual_user_role.id)
-    assert body['name'] == usual_user_role.name
+    assert body['id'] == str(role.id)
+    assert body['name'] == role.name
 
 
 @pytest.mark.asyncio
-async def test_assign_existing_role_to_another_by_usual_user(
+async def test_assign_existing_role_to_another_by_regular_user(
         client: Client,
+        superuser: TestUser,
         user: TestUser,
-        usual_user_role: TestRole
+        role: Role
 ) -> None:
-    access_token, _ = await login(user, client, user_agent=f'test user agent {uuid4()}')
-    user_to_assign_role = TestUser(id=uuid4(), email='user2@test.com', password='password')
+    access_token, _ = await login(user, client)
 
     response = await client.post(
-        f'api/v1/users/{user_to_assign_role.id}/roles?role_id={usual_user_role.id}',
+        f'api/v1/users/{superuser.id}/roles?role_id={role.id}',
         headers=build_headers(access_token)
     )
 
@@ -104,7 +128,7 @@ async def test_assign_existing_role_to_another_by_usual_user(
 
 @pytest.mark.asyncio
 async def test_assign_not_existing_role(client: Client, superuser: TestUser) -> None:
-    access_token, _ = await login(superuser, client, user_agent=f'test user agent {uuid4()}')
+    access_token, _ = await login(superuser, client)
 
     response = await client.post(
         f'api/v1/users/{superuser.id}/roles?role_id={uuid4()}',
@@ -119,12 +143,11 @@ async def test_dissociate_existing_role_to_another_by_superuser(
         client: Client,
         superuser: TestUser,
         user: TestUser,
-        usual_user_role: TestRole
 ) -> None:
-    access_token, _ = await login(superuser, client, user_agent=f'test user agent {uuid4()}')
+    access_token, _ = await login(superuser, client)
 
     response = await client.delete(
-        f'api/v1/users/{user.id}/roles?role_id={usual_user_role.id}',
+        f'api/v1/users/{user.id}/roles?role_id={user.roles[0].id}',
         headers=build_headers(access_token)
     )
 
@@ -135,13 +158,12 @@ async def test_dissociate_existing_role_to_another_by_superuser(
 async def test_dissociate_existing_role_to_another_by_usual_user(
         client: Client,
         user: TestUser,
-        usual_user_role: TestRole
+        superuser: TestUser
 ) -> None:
-    access_token, _ = await login(user, client, user_agent=f'test user agent {uuid4()}')
-    user_to_dissociate_role = TestUser(id=uuid4(), email='user2@test.com', password='password')
+    access_token, _ = await login(user, client)
 
     response = await client.post(
-        f'api/v1/users/{user_to_dissociate_role.id}/roles?role_id={usual_user_role.id}',
+        f'api/v1/users/{superuser.id}/roles?role_id={superuser.roles[0].id}',
         headers=build_headers(access_token)
     )
 
@@ -150,7 +172,7 @@ async def test_dissociate_existing_role_to_another_by_usual_user(
 
 @pytest.mark.asyncio
 async def test_dissociate_not_existing_role(client: Client, superuser: TestUser) -> None:
-    access_token, _ = await login(superuser, client, user_agent=f'test user agent {uuid4()}')
+    access_token, _ = await login(superuser, client)
 
     response = await client.post(
         f'api/v1/users/{superuser.id}/roles?role_id={uuid4()}',
