35e044df17a8399aac9cbaeefbba5116507d6311
iKonstantin1991 <ikonstantin1991@mail.ru>
2024-06-06 11:32:13 +0300

    improve db models (#28)

--- auth-service/src/models/entity.py
+++ auth-service/src/models/entity.py
@@ -2,7 +2,7 @@ import uuid
 from typing import List
 from datetime import datetime
 
-from sqlalchemy import Column, Table, ForeignKey, func
+from sqlalchemy import Column, Table, ForeignKey, Text, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.dialects.postgresql import UUID
 
@@ -12,8 +12,8 @@ from db.postgres import Base
 user_role = Table(
     'user_role',
     Base.metadata,
-    Column('user_id', ForeignKey('users.id'), primary_key=True),
-    Column('role_id', ForeignKey('roles.id'), primary_key=True),
+    Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), primary_key=True),
+    Column('role_id', ForeignKey('roles.id', ondelete='CASCADE'), primary_key=True),
 )
 
 
@@ -21,8 +21,8 @@ class User(Base):
     __tablename__ = 'users'
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
-    email: Mapped[str] = mapped_column(unique=True, nullable=False)
-    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(Text, unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(Text, nullable=False)
     created: Mapped[datetime] = mapped_column(insert_default=func.now())  # pylint: disable=not-callable
     logins: Mapped[List['UserLogin']] = relationship(back_populates='user')
     roles: Mapped[List['Role']] = relationship(secondary=user_role, back_populates='users')
@@ -35,7 +35,7 @@ class Role(Base):
     __tablename__ = 'roles'
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
-    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(Text, unique=True, nullable=False)
     created: Mapped[datetime] = mapped_column(insert_default=func.now())  # pylint: disable=not-callable
     users: Mapped[List['User']] = relationship(secondary=user_role, back_populates='roles')
 
@@ -47,9 +47,9 @@ class UserLogin(Base):
     __tablename__ = 'user_logins'
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
-    user_agent: Mapped[str | None]
+    user_agent: Mapped[str | None] = mapped_column(Text)
     date: Mapped[datetime] = mapped_column(nullable=False)
-    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('users.id'))
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'))
     user: Mapped['User'] = relationship(back_populates='logins')
 
     def __repr__(self) -> str:
